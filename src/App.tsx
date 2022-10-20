a7d9203e90c36f289f46b24441db2f92f0adc86c
Aliia Fomenko <418420a@gmail.com>
2022-10-20 20:52:48 +0600

    Написана функция создания и присваивания рандомных чисел. Добавлены css стили для офрмления div App

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,28 @@ import './App.css';
 import Lottery from "./Lottery/Lottery";
 
 function App() {
-    const randomNumber1 = Math.floor(Math.random() * (12 - 5)) + 5;
-    const randomNumber2 = Math.floor(Math.random() * (18 - 12)) + 12;
-    const randomNumber3 = Math.floor(Math.random() * (24 - 18)) + 18;
-    const randomNumber4 = Math.floor(Math.random() * (30 - 24)) + 24;
-    const randomNumber5 = Math.floor(Math.random() * (36 - 30)) + 30;
+  const randomNumber1 = Math.floor(Math.random() * (12 - 5)) + 5;
+  const randomNumber2 = Math.floor(Math.random() * (18 - 12)) + 12;
+  const randomNumber3 = Math.floor(Math.random() * (24 - 18)) + 18;
+  const randomNumber4 = Math.floor(Math.random() * (30 - 24)) + 24;
+  const randomNumber5 = Math.floor(Math.random() * (36 - 30)) + 30;
+
+  function refreshPage(){
+    window.location.reload();
+  }
 
   return (
     <div className="App">
-      <Lottery number={randomNumber1}/>
-      <Lottery number={randomNumber2}/>
-      <Lottery number={randomNumber3}/>
-      <Lottery number={randomNumber4}/>
-      <Lottery number={randomNumber5}/>
-      <div>
-        <button>Change name!</button>
+      <div className="Number">
+        <div className="lottery">Lottery!!! Try your luck!!!</div>
+        <Lottery number={randomNumber1}/>
+        <Lottery number={randomNumber2}/>
+        <Lottery number={randomNumber3}/>
+        <Lottery number={randomNumber4}/>
+        <Lottery number={randomNumber5}/>
+        <div>
+          <button type="button" className="btn" onClick={refreshPage}>Change number!</button>
+        </div>
       </div>
     </div>
   );
